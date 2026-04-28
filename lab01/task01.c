d63e3eb0d3206fd90673b2aa26b699d72ef9693b
gana <113369609+Ganathipan@users.noreply.github.com>
2026-04-28 19:43:11 +0530

    LAB01: Task  initial LED blinking program in and .gitignore for output files

---
+++ lab01/task01.c
@@ -0,0 +1,44 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BLINK_DELAY_MS 10001
+
+int main(void)
+{
+    // configure pins of PORTB for output
+    DDRB = DDRB | (1 << 5);
+    DDRB = DDRB | (1 << 4);
+    DDRB = DDRB | (1 << 3);
+    DDRB = DDRB | (1 << 2);
+    DDRB = DDRB | (1 << 1);
+
+    // initialize variables for controlling the LED sequence
+    int lf = 1, rg = 5, cur = 1, pre = 1, dir = 1;
+    
+    // set all pins of PORTB low to turn all leds off
+    PORTB = 0x00;
+
+    while (lf <= cur && cur <= rg) {
+        PORTB = PORTB | (1 << cur);
+        _delay_ms(BLINK_DELAY_MS);
+
+        if (cur != pre) {
+            PORTB = PORTB & ~(1 << pre);
+            _delay_ms(BLINK_DELAY_MS);
+        }
+        pre = cur;
+        cur = cur + dir;
+        
+        if (cur > rg) {
+            dir = -1;
+            rg = rg - 1;
+            cur = rg;
+        } else if (cur < lf) {
+            dir = 1;
+            lf = lf + 1;
+            cur = lf;
+        }
+    }
+    PORTB = 0x00; // turn all leds off at the end of the sequence
+    return 0;
+}
